761ebd55e6a930cf55a185f57d387ab04dc2984c
RealLifeDeveloper <rld@reallifedeveloper.com>
2026-09-16 09:46:20 +0200

    Added missing import of java.util.Optional in PredicateSpecificationEvaluator

--- src/main/java/com/reallifedeveloper/tools/test/database/inmemory/PredicateSpecificationEvaluator.java
+++ src/main/java/com/reallifedeveloper/tools/test/database/inmemory/PredicateSpecificationEvaluator.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 
